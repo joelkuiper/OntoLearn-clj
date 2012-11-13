e4a4a6d6dfa89adec05564f56997fdd9b70a9fc1
Joel Kuiper <me@joelkuiper.eu>
2012-11-13 10:42:17 +0100

    Minor fixes

--- src/indications/preprocess/tfidf.clj
+++ src/indications/preprocess/tfidf.clj
@@ -3,6 +3,7 @@
             [java.lang.Math]))
 
 (defn tf-idf [doc#]
+  "See http://en.wikipedia.org/wiki/Tf%E2%80%93idf"
   (fn [term# token# docs-with-term#]
     (let [tf (/ term# (+ token# Double/MIN_VALUE))
           idf (Math/log10 (/ doc# (+ Double/MIN_VALUE docs-with-term#)))]
