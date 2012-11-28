761a6499c3a929cde880530a0a4f96a1d94b89a4
Joël Kuiper <me@joelkuiper.eu>
2012-11-28 23:53:36 +0100

    Update import script to ignore Disease Ontology IDs

--- src/indications/import/process.clj
+++ src/indications/import/process.clj
@@ -13,20 +13,21 @@
   (let [pmid (num (Integer/parseInt (xp/$x:text? ".//MedlineCitation/PMID" node)))]
     (if (= (wcar (car/sismember "all" pmid)) 0)
       (let [mesh-terms (xp/$x:text* ".//MeshHeading//DescriptorName" node)
-            disease-ids (doids mesh-terms)
+            ;disease-ids (doids mesh-terms)
             title (apply str (xp/$x:text* ".//ArticleTitle" node))
             abstracts (xp/$x:text* ".//Abstract/AbstractText" node)]
-        (wcar (doall (map (fn [doid] 
-                            (car/sadd (str "doid:" doid) pmid)
-                            (car/sadd "diseases" doid)
-                            (doall (map #(car/sadd (str "mesh:" pmid) %) mesh-terms))
-                            (doall (map #(car/sadd (str "mesh:" %) pmid) mesh-terms))
-                            (doall (map #(car/sadd (str "xrefs:" doid) %) (annotations "xref" [doid])))
-                            (car/sadd (str "pmid:" pmid) doid)) disease-ids))
+        (wcar ;(doall (map (fn [doid] 
+              ;              (car/sadd (str "doid:" doid) pmid)
+              ;              (car/sadd "diseases" doid)
+              ;              (doall (map #(car/sadd (str "xrefs:" doid) %) (annotations "xref" [doid])))
+              ;              (car/sadd (str "pmid:" pmid) doid)) disease-ids))
               (car/hset "abstracts" pmid (str title ",  " (strs/join " " abstracts)))
+              (doall (map #(car/sadd (str "mesh:" pmid) %) mesh-terms))
+              (doall (map #(car/sadd (str "mesh:" %) pmid) mesh-terms))
               (car/sadd "all" pmid)
               (when-not (empty? mesh-terms) (car/sadd "mesh_annotated" pmid))
-              (when-not (empty? disease-ids) (car/sadd "annotated" pmid)))))))
+              ; (when-not (empty? disease-ids) (car/sadd "annotated" pmid))
+              )))))
 
 (defn- import-publications [nodes] 
   (doall (map import-publication nodes)))
@@ -48,6 +49,6 @@
     (if (empty? files)
       (println "Please supply a valid directory with PubMed XML files") 
       (do 
-        (swap! ontology assoc :mesh->doid (mesh-index @ontology))
+        ;(swap! ontology assoc :mesh->doid (mesh-index @ontology))
         (import-files (rest files))))))
 
